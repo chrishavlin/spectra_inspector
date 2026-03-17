f592dcfae2b56439aac40ab84030a58625504030
chris <chrish.havlin@gmail.com>
2026-03-17 11:36:15 -0500

    refactor bitmaps, add and remove bitmaps

--- src/spectra_inspector/components/__init__.py
+++ src/spectra_inspector/components/__init__.py
@@ -1,1 +1,4 @@
 from .dataset_selector import dataset_selector
+from .bitmap_image import bitmap_image_layout, bitmapImageLayoutIDs
+
+__all__ = ['bitmap_image_layout', 'bitmapImageLayoutIDs', 'dataset_selector']
