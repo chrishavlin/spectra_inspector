8ea8710dfdd55f09abdc9cc7382d4821b0159901
chrishavlin <chris.havlin@gmail.com>
2026-03-25 16:24:05 -0500

    update element names, add 3 images initially

--- src/spectra_inspector/utilities/element_energy_ranges.py
+++ src/spectra_inspector/utilities/element_energy_ranges.py
@@ -2,8 +2,7 @@
 # determines excitation/emission level.
 # see https://en.wikipedia.org/wiki/Energy-dispersive_X-ray_spectroscopy
 element_energy_ranges_keV = {
-    "element-0": (0.96, 1.12),
-    "element-1": (1.2, 1.3),
-    "element-2": (1.4, 1.6),
-    "element-3": (1.65, 1.85),
+    "Mg": (1.16, 1.34),
+    "Al": (1.4, 1.6),
+    "Si": (1.64, 1.9),
 }
