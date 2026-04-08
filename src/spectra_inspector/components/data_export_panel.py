169c2c23f2425ae1b6b2c5a79bff04c95b712b2a
chrishavlin <chris.havlin@gmail.com>
2026-04-08 14:19:28 -0500

    add export panel (summary PDF/zip, MSA)

---
+++ src/spectra_inspector/components/data_export_panel.py
@@ -0,0 +1,123 @@
+import dash_bootstrap_components as dbc
+from dash import dcc, html
+
+from spectra_inspector.components.layout_ids import indexedLayoutIDMapper
+
+
+class dataExportPanelIDS(indexedLayoutIDMapper):
+    prop_names: tuple[str, ...] = (
+        "div",
+        "formatdropdown",
+        "slider",
+        "exportsummary",
+        "exportmsa",
+        "downloadsummary",
+        "downloadmsa",
+        "msafileformat",
+    )
+
+    def __init__(
+        self, id_type_base: str = "data-export-panel", index: int | None = None
+    ):
+        super().__init__(id_type_base, index)
+
+    @property
+    def formatdropdown(self) -> str:
+        return self.full_id("-formatdropdown")
+
+    @property
+    def exportsummary(self) -> str:
+        return self.full_id("-exportsummarybutton")
+
+    @property
+    def exportmsa(self) -> str:
+        return self.full_id("-exportmsa")
+
+    @property
+    def downloadsummary(self) -> str:
+        return self.full_id("-downloadsummary")
+
+    @property
+    def downloadmsa(self) -> str:
+        return self.full_id("-downloadmsa")
+
+    @property
+    def msafileformat(self) -> str:
+        return self.full_id("-msafileformat")
+
+
+def get_layout(
+    id_type_base: str = "data-export-panel",
+    index: int = 0,
+) -> tuple[dbc.Container, dataExportPanelIDS]:
+
+    layoutIDs = dataExportPanelIDS(id_type_base, index=index)
+
+    summary_row = dbc.Row(
+        [
+            dbc.Col(dcc.Markdown("format:"), width=4),
+            dbc.Col(
+                dcc.Dropdown(
+                    [".zip", "PDF"],
+                    value=".zip",
+                    id=layoutIDs.formatdropdown,
+                    className="text-info",
+                    searchable=False,
+                ),
+                width=4,
+            ),
+            dbc.Col(
+                dbc.Button(
+                    "Export", id=layoutIDs.exportsummary, n_clicks=0, color="secondary"
+                ),
+                width=4,
+            ),
+        ],
+    )
+
+    msa_row = dbc.Row(
+        [
+            dbc.Col(dcc.Markdown("MSA format:"), width=4),
+            dbc.Col(
+                dcc.Dropdown(
+                    ["Y", "XY"],
+                    value="XY",
+                    id=layoutIDs.msafileformat,
+                    className="text-info",
+                    searchable=False,
+                ),
+                width=4,
+            ),
+            dbc.Col(
+                dbc.Button(
+                    "Export MSA", id=layoutIDs.exportmsa, n_clicks=0, color="secondary"
+                ),
+                width=4,
+            ),
+        ]
+    )
+
+    card = dbc.Card(
+        dbc.CardBody(
+            [
+                html.H3("Extract-a-comp!", className="card-title"),
+                html.Hr(),
+                html.H5("Export summary", className="card-subtitle"),
+                dbc.Container(summary_row),
+                html.Hr(),
+                html.H5("Export MSA", className="card-subtitle"),
+                dbc.Container(msa_row),
+            ]
+        ),
+        color="primary",
+        inverse=True,
+    )
+
+    rows = []
+    rows.append(card)
+    rows.append(dcc.Download(id=layoutIDs.downloadsummary))
+    rows.append(dcc.Download(id=layoutIDs.downloadmsa))
+
+    cont = dbc.Container(rows, style={"width": "32rem"})
+
+    return cont, layoutIDs
---
+++ src/spectra_inspector/utilities/summary_writer.py
@@ -0,0 +1,168 @@
+import datetime
+import shutil
+import uuid
+from pathlib import Path
+from typing import Literal
+from zoneinfo import ZoneInfo
+
+import fpdf
+import plotly
+
+from spectra_inspector.logging import spectraLogger
+from spectra_inspector.settings import Settings
+
+
+class summaryWriter:
+    folder_name: Path = Path("spector-inspector")
+    pdf_name: Path = Path("SpectraInspectorSummary.pdf")
+    parent_write_dir: Path
+    unique_write_dir: Path
+    settings: Settings
+
+    def __init__(
+        self,
+        folder_name: Path | str = "spector_inspector",
+        cleanup_tmp_dirs: bool = True,
+        settings: Settings | None = None,
+    ):
+        if settings is None:
+            settings = Settings()
+
+        self.settings = settings
+        self.unique_write_dir = Path(uuid.uuid4().hex)
+        self.parent_write_dir = Path(self.settings.write_dir)
+        self.folder_name = Path(folder_name)
+
+        if self.parent_write_dir.is_dir() is False:
+            self.parent_write_dir.mkdir()
+
+        if cleanup_tmp_dirs:
+            self.clean_parent()
+
+        # the unique subdir under which we write this write session's files
+        uniq = self.parent_write_dir / self.unique_write_dir
+        uniq.mkdir()
+
+        # the full write directory
+        w = self.write_dir
+        w.mkdir()
+
+    def clean_parent(self):
+        max_dirs = self.settings.max_tmp_dirs
+
+        dirs = [f for f in self.parent_write_dir.glob("*") if f.is_dir()]
+        n_dirs_to_rm = len(dirs) - max_dirs
+        if n_dirs_to_rm > 0:
+            mod_times = [f.stat().st_mtime_ns for f in dirs]
+            mod_times.sort(reverse=True)
+            delete_these = [
+                f for _, f in sorted(zip(mod_times, dirs, strict=True), reverse=True)
+            ][:n_dirs_to_rm]
+            for f in delete_these:
+                shutil.rmtree(f)
+            spectraLogger.info(f"deleted {len(delete_these)} temp directories")
+
+    @property
+    def write_dir(self) -> Path:
+        return self.parent_write_dir / self.unique_write_dir / self.folder_name
+
+    def full_file(self, f: str | Path) -> Path:
+        return self.write_dir / Path(f)
+
+    def write_static_figures(
+        self,
+        figures: dict[str, plotly.graph_objs.Figure],
+        figformat: Literal["png", "svg", "pdf"] = "png",
+    ):
+
+        fig_files = [self.full_file(f"{f}.{figformat}") for f in figures]
+        fig_list = list(figures.values())
+        plotly.io.write_images(fig_list, fig_files, format=figformat)
+        spectraLogger.info(f"wrote image files to {self.write_dir}")
+
+    def get_zip(self, include_pdf: bool = False) -> Path:
+
+        if include_pdf:
+            self.write_pdf()
+
+        parent = self.parent_write_dir / self.unique_write_dir
+        zip_fi = parent / "spector-inspector-summary"
+        zfilename = shutil.make_archive(str(zip_fi), "zip", root_dir=self.write_dir)
+
+        return Path(zfilename)
+
+    def write_pdf(
+        self,
+    ):
+
+        files = [f for f in self.write_dir.glob("*") if f.is_file()]
+
+        bitmaps = []
+        spectrum = []
+        for f in files:
+            if f.stem.startswith("bitmap"):
+                bitmaps.append(self.write_dir / f)
+            elif f.stem.startswith("spectr"):
+                spectrum.append(self.write_dir / f)
+
+        pdf = fpdf.FPDF(orientation="portrait", format="A4")
+        pdf.set_font("Helvetica", size=18)
+
+        pdf.add_page()
+        lines = [
+            "SpectorInspector auto-generated summary PDF ",
+            f"Generated: {datetime.datetime.now(tz=ZoneInfo('UTC'))}",
+        ]
+
+        pdf.write(text="\n".join(lines))
+
+        pdf.add_page()
+        pdf.cell(text="Sample metadata:")
+
+        for fname in bitmaps + spectrum:
+            pdf.add_page()
+            pdf.write(text=f"{fname.stem}\n\n")
+            pdf.image(fname, w=pdf.epw)
+
+        fout = self.full_file(self.pdf_name)
+        pdf.output(fout)
+
+    def get_pdf_path(self, generate_pdf: bool = True):
+        fout = self.full_file(self.pdf_name)
+        if fout.is_file() is False and generate_pdf:
+            self.write_pdf()
+
+        return fout
+
+    def write_MSA(
+        self,
+        active_spectrum_metadata: dict,
+        file_format: Literal["Y", "XY"] | None = None,
+    ) -> Path:
+        from rsciio.msa import file_writer
+
+        intensity = active_spectrum_metadata["intensity"]
+        energy = active_spectrum_metadata["energy"]
+        attrs = active_spectrum_metadata["attrs"]
+
+        signal = {
+            "data": intensity,
+            "axes": [
+                {
+                    "size": len(intensity),
+                    "index_in_array": 0,
+                    "name": "Energy",
+                    "scale": energy[1] - energy[0],
+                    "offset": energy[0],
+                    "units": "keV",
+                    "navigate": False,
+                }
+            ],
+            "metadata": attrs["metadata"],
+            "original_metadata": attrs["original_metadata"],
+        }
+        f = self.full_file("spectrum.msa")
+
+        file_format = file_format or "XY"
+        file_writer(f, signal, format=file_format)
+        return f
