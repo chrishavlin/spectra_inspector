ba84bc0b75552af1ff86ef5326222d43ac030b18
chavlin <chris.havlin@gmail.com>
2026-07-17 14:16:41 -0500

    does this work

--- packages/spectra_inspector_server/docker-entrypoint.sh
+++ packages/spectra_inspector_server/docker-entrypoint.sh
@@ -1,5 +1,2 @@
 #!/bin/sh
-exec uv run fastapi run \
-  --host 0.0.0.0 \
-  --workers "${SPECTRA_INSPECTOR_DOCKER_FASTAPI_WORKERS:-1}" \
-  src/spectra_inspector_server/main.py
+exec uv run fastapi run --host 0.0.0.0 --workers "${SPECTRA_INSPECTOR_DOCKER_FASTAPI_WORKERS:-1}" src/spectra_inspector_server/main.py
